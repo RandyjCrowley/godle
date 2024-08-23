a1dc01f09db3ee4d737ff88cebe247909929e987
Randy Crowley <Randy.Crowley@icloud.com>
2024-08-22 21:40:34 -0500

    added word file saving to check if exists

--- wordle/wordle.go
+++ wordle/wordle.go
@@ -1,9 +1,13 @@
 package wordle
 
 import (
-//	"encoding/json"
+	//	"encoding/json"
 	"net/http"
+	"os"
+    "strings"
+    "bufio"
 
+    "fmt"
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
@@ -16,6 +20,44 @@ type WordleResponse struct {
 func Start(c echo.Context) error {
 	id := uuid.Must(uuid.NewRandom()).String()
 	word := get_word()
+    AddWord(word)
 	response := WordleResponse{ID: id, Word: word}
 	return c.JSON(http.StatusOK, response)
 }
+
+func GetFile() (*os.File) {
+    file, err := os.OpenFile("word.list", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        fmt.Println(err)
+        return nil
+    }
+
+    return file
+}
+
+func CheckWord(word string) bool {
+    file := GetFile()
+    
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), word) {
+			return true
+		}
+	}
+    return false
+}
+
+func AddWord(word string) bool {
+    file := GetFile()
+    if (! CheckWord(word)) {
+        _, err := file.WriteString(word)
+        if err != nil {
+            fmt.Println("Error writing to file:", err)
+        }
+        defer file.Close()
+        return true
+    }
+    defer file.Close()
+    return false
+}
+
